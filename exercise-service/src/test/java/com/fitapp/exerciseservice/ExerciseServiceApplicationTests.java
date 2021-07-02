08f0056a15842b5281761301825b7a9011994113
rikp777 <rikpeeters11@hotmail.com>
2021-07-02 12:00:48 +0200

    Initial commit

---
+++ exercise-service/src/test/java/com/fitapp/exerciseservice/ExerciseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitapp.exerciseservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
